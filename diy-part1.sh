95ae8d3a26232b362268b99de5ebe49cbf4c697d
lsydy1021 <59816384+lsydy1021@users.noreply.github.com>
2021-02-26 14:30:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,5 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+src-git helloworld https://github.com/fw876/helloworld
+git clone -b master https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
